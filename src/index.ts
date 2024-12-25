32d81d232aa6865f1309a0a2d9a241dc738a4c64
netoabel <abel.neto@gmail.com>
2024-12-25 14:53:24 -0300

    adds support to setting temperature

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,8 @@ export interface State extends Partial<KeyLightState> {}
 export class KeyLight {
   private readonly MIN_BRIGHTNESS = 2;
   private readonly MAX_BRIGHTNESS = 100;
+  private readonly MIN_TEMPERATURE = 2900;
+  private readonly MAX_TEMPERATURE = 7000;
   private readonly REQUEST_TIMEOUT_MS = 3000;
   private readonly agentForHttp4: http.Agent;
   private readonly url: string;
@@ -62,5 +64,23 @@ export class KeyLight {
     }
     const res = await axios.get<LightResponse>(this.url, config);
     return res.data.lights[0];
+  }
+
+  async setTemperature(temperature: number): Promise<void> {
+    await this.setState({ temperature });
+  }
+
+  async increaseTemperature(by: number): Promise<void> {
+    const state = await this.getCurrentState();
+    let temperature = state.temperature + by;
+    if (temperature > this.MAX_TEMPERATURE) temperature = this.MAX_TEMPERATURE;
+    await this.setState({ temperature });
+  }
+
+  async decreaseTemperature(by: number): Promise<void> {
+    const state = await this.getCurrentState();
+    let temperature = state.temperature - by;
+    if (temperature < this.MIN_TEMPERATURE) temperature = this.MIN_TEMPERATURE;
+    await this.setState({ temperature });
   }
 }
